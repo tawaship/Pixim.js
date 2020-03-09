3214fd233d0278e437e91647ddb2e1550795f759
tawaship <makazu.mori@gmail.com>
2020-03-09 12:13:01 +0900

    export basement manifest class

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './modules/Application';
 export * from './modules/Container';
 export * from './modules/Content';
 export * from './modules/Emitter';
+export * from './modules/ContentManifestBase';
 
 import { Content } from './modules/Content';
 
